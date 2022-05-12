0f84281c273fec90ff306025231405151c9f03cd
qui3tK1D <zawhlyanhtet@gmail.com>
2022-05-12 23:53:24 +0630

    layout almost

--- js/app.js
+++ js/app.js
@@ -38,24 +38,3 @@ const slideDownMain = {
   delay: 100,
 };
 ScrollReveal().reveal(".mainContent", slideDownMain);
-
-const btnRotate = document.querySelector(".rotateBtn");
-
-const rotate = function () {
-  btnRotate.querySelector(".fa-solid").style.transition = "1s";
-  btnRotate.querySelector(".fa-solid").style.transform = `rotate(${this}deg)`;
-};
-
-btnRotate.addEventListener("mouseover", rotate.bind("360"));
-btnRotate.addEventListener("mouseout", rotate.bind("-360"));
-
-const arrowMove = document.querySelectorAll(".arrowMove");
-arrowMove.forEach((cur) => {
-  cur.addEventListener("mouseover", function () {
-    cur.querySelector(".fa-arrow-right").style.transition = "1s";
-    cur.querySelector(".fa-arrow-right").style.marginLeft = "6px";
-  });
-  cur.addEventListener("mouseout", function () {
-    cur.querySelector(".fa-arrow-right").style.marginLeft = "0";
-  });
-});
